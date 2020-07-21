a93f04cb4709bf277f681790ec4146754bc90993
i1-kmv <i1-kmv@mail.ru>
2020-07-21 19:21:33 +0200

    Updates

---
+++ precache-manifest.91d552f9665e1237c13b59af85eb128a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8241d8a98b7ae142e51287f116baf6e",
+    "url": "/React-resto-app/./index.html"
+  },
+  {
+    "revision": "fb042e0ee56c3c9dd542",
+    "url": "/React-resto-app/./static/css/main.026fc84c.chunk.css"
+  },
+  {
+    "revision": "72a8b06b803a9759d88e",
+    "url": "/React-resto-app/./static/js/2.a0e99cb2.chunk.js"
+  },
+  {
+    "revision": "fb042e0ee56c3c9dd542",
+    "url": "/React-resto-app/./static/js/main.3a3553fe.chunk.js"
+  },
+  {
+    "revision": "7f3eee3ed0d3ac8e19e3",
+    "url": "/React-resto-app/./static/js/runtime~main.c1a1446c.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/React-resto-app/./static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/React-resto-app/./static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
